3743cb549f728b3c35aea24091ec0e9f2b001651
Steven Sun <gsuuon@gmail.com>
2023-04-28 19:03:22 -0500

    feat: add Utility library

---
+++ src/Command.Utility/Library.fs
@@ -0,0 +1,20 @@
+﻿module Gsuuon.Command.Utility
+
+open System
+
+/// Prints using alternating printers and includes line number
+/// Useful for debugging per-read output
+let lineNumAlternating printA printB =
+    let mutable count = 0
+
+    fun (msg: string) ->
+        let printer =
+            if count % 2 = 0 then printA
+            else printB
+
+        printer $"%3d{count}|{msg}"
+
+        count <- count + 1
+
+/// Blocking sleep for ms
+let sleep ms = Threading.Thread.Sleep 1000
